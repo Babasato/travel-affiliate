323da9d9494f66a1aa5c52104d67bba651f17478
Babasato <114899243+Babasato@users.noreply.github.com>
2024-11-09 21:32:01 +0300

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <main>
+      <h1>Welcome to My Travel Site</h1>
+      <p>Find amazing deals on hotels, flights, and more!</p>
+    </main>
+  );
+};
+
+export default HomePage;
